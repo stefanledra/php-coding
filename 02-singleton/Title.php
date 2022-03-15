44133d96d3e8b74dde1e21b6e4963d1ce1028507
stefanledra <101395559+stefanledra@users.noreply.github.com>
2022-03-15 13:08:45 +0100

    Improvements to singleton, completed error handling

---
+++ 02-singleton/Title.php
@@ -0,0 +1,11 @@
+<?php
+
+enum Title : string {
+
+    case hr = 'HR';
+    case leadDev = 'Lead Developer';
+    case seniorDev = 'Senior Developer';
+    case juniorDev = 'Junior Developer' ;
+    case intern = 'Intern' ;
+
+}
