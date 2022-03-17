8a61f57758b85ecca5e6e547e7a50a5c4e6c0aa2
stefanledra <101395559+stefanledra@users.noreply.github.com>
2022-03-17 16:07:17 +0100

    Changes made to constructor,encryptio now conditional

--- 03-hashing-openssl/Hashing-openssl.php
+++ 03-hashing-openssl/Hashing-openssl.php
@@ -3,25 +3,45 @@
 class Encryption
 {
 
-    private string $cipher = 'aes-256-cbc';
-    private int $option = 0;
+    private ?string $cipher;
+    private array $cipherMethods;
+    private int $option;
 
 
-    public function __construct()
+    public function __construct($cipher)
     {
+        $this->cipherMethods = openssl_get_cipher_methods();
+        if (in_array($cipher, $this->cipherMethods)) {
+            $this->cipher = $cipher;
+            $this->option = 0;
+        } else {
+            $this->cipher = null;
+            echo 'Invalid encryption method, please create a new object with a valid encryption method!';
+        }
 
     }
 
     public function encrypt($data): array
     {
-        $keyGenBits    = 16;
-        $key           = bin2hex(openssl_random_pseudo_bytes($keyGenBits));
-        $ivlen         = openssl_cipher_iv_length($this->cipher);
-        $iv            = openssl_random_pseudo_bytes($ivlen);
-        $encryptedData = openssl_encrypt($data, $this->cipher, $key, $this->option, $iv);
-        $resultArray   = array('encryptedData' => $encryptedData, 'key' => $key, 'iv' => $iv);
+        if (is_null($this->cipher)) {
+            echo 'Can not encrypt with an invalid method!';
 
-        return $resultArray;
+            return array();
+        } else {
+            $keyGenBits = 16;   // Number of bits needed for the key to be valid
+            $key = bin2hex(openssl_random_pseudo_bytes($keyGenBits));       //Using the functions to get a randomized and valid key for encryption
+            $ivlen = openssl_cipher_iv_length($this->cipher);
+            $iv = openssl_random_pseudo_bytes($ivlen);
+            $encryptedData = openssl_encrypt($data, $this->cipher, $key, $this->option, $iv);
+            $resultArray = array(
+                'encryptedData' => $encryptedData,
+                'key'           => $key,
+                'iv'            => $iv,
+                'cipher'        => $this->cipher,
+            );
+
+            return $resultArray;
+        }
     }
 
     public function decrypt($dataArray): string
@@ -29,7 +49,7 @@ class Encryption
 
         $decryptedData = openssl_decrypt(
             $dataArray['encryptedData'],
-            $this->cipher,
+            $dataArray['cipher'],
             $dataArray['key'],
             $this->option,
             $dataArray['iv']);
@@ -41,9 +61,13 @@ class Encryption
 
 $dataToEncrypt = 'This is to be encrypted';
 $dataToDecrypt = array();
-$new           = new Encryption();
+$cipher        = 'aes-192-cbc';
+$new           = new Encryption($cipher);
 $dataToDecrypt = $new->encrypt($dataToEncrypt);
-echo 'Data after encrypting: '.$dataToDecrypt['encryptedData'].'<br />';
+echo 'Encrypted data: '.$dataToDecrypt['encryptedData'].'<br />'.
+     'Key you can use for decrypting: '.$dataToDecrypt['key'].'<br />'.
+     'Initialisation vector: '.$dataToDecrypt['iv'].'<br />'.
+     'Encryption method: '.$dataToDecrypt['cipher'].'<br />';
+echo 'Data after decrypting back: '.$new->decrypt($dataToDecrypt).'<br />';
 
-echo 'Data after decripting back: '.$new->decrypt($dataToDecrypt).'<br />';
 
