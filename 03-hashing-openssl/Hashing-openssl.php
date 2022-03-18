5fee92d34ca38d6d3c257d61829d5bacd0b239e1
stefanledra <101395559+stefanledra@users.noreply.github.com>
2022-03-18 11:06:58 +0100

    Error handling and finishing changes

--- 03-hashing-openssl/Hashing-openssl.php
+++ 03-hashing-openssl/Hashing-openssl.php
@@ -3,71 +3,65 @@
 class Encryption
 {
 
-    private ?string $cipher;
+    private string $cipher;
+    private string $key;
+    private string $iv;
+    private string $initialWord;
     private array $cipherMethods;
     private int $option;
+    private int $keyGenBits;
 
 
     public function __construct($cipher)
     {
+        $this->keyGenBits    = 16;   // Number of bits needed for the key to be valid
+        $this->option        = 0; // constant, should always be 0
         $this->cipherMethods = openssl_get_cipher_methods();
         if (in_array($cipher, $this->cipherMethods)) {
             $this->cipher = $cipher;
-            $this->option = 0;
+
         } else {
-            $this->cipher = null;
-            echo 'Invalid encryption method, please create a new object with a valid encryption method!';
+            throw new Exception('Invalid encryption method!');
         }
 
     }
 
-    public function encrypt($data): array
+    public function encrypt($initialWord): string
     {
-        if (is_null($this->cipher)) {
-            echo 'Can not encrypt with an invalid method!';
 
-            return array();
-        } else {
-            $keyGenBits = 16;   // Number of bits needed for the key to be valid
-            $key = bin2hex(openssl_random_pseudo_bytes($keyGenBits));       //Using the functions to get a randomized and valid key for encryption
-            $ivlen = openssl_cipher_iv_length($this->cipher);
-            $iv = openssl_random_pseudo_bytes($ivlen);
-            $encryptedData = openssl_encrypt($data, $this->cipher, $key, $this->option, $iv);
-            $resultArray = array(
-                'encryptedData' => $encryptedData,
-                'key'           => $key,
-                'iv'            => $iv,
-                'cipher'        => $this->cipher,
-            );
+        $this->key         = bin2hex(openssl_random_pseudo_bytes($this->keyGenBits));       //Using the functions to get a randomized and valid key for encryption
+        $ivlen             = openssl_cipher_iv_length($this->cipher);
+        $this->iv          = openssl_random_pseudo_bytes($ivlen);
+        $this->initialWord = $initialWord;
 
-            return $resultArray;
-        }
+        return openssl_encrypt($initialWord, $this->cipher, $this->key, $this->option, $this->iv);
+
     }
 
-    public function decrypt($dataArray): string
+    public function decrypt($encryptedWord): string
     {
-
-        $decryptedData = openssl_decrypt(
-            $dataArray['encryptedData'],
-            $dataArray['cipher'],
-            $dataArray['key'],
-            $this->option,
-            $dataArray['iv']);
-
-        return $decryptedData;
-
+        $decryptedWord = openssl_decrypt($encryptedWord, $this->cipher, $this->key, $this->option, $this->iv);
+        if ($decryptedWord == $this->initialWord) {
+            return $decryptedWord;
+        } else {
+            return 'Provided word can not be decrypted. Please input the valid ciphertext!';
+        }
     }
 }
 
-$dataToEncrypt = 'This is to be encrypted';
-$dataToDecrypt = array();
-$cipher        = 'aes-192-cbc';
-$new           = new Encryption($cipher);
-$dataToDecrypt = $new->encrypt($dataToEncrypt);
-echo 'Encrypted data: '.$dataToDecrypt['encryptedData'].'<br />'.
-     'Key you can use for decrypting: '.$dataToDecrypt['key'].'<br />'.
-     'Initialisation vector: '.$dataToDecrypt['iv'].'<br />'.
-     'Encryption method: '.$dataToDecrypt['cipher'].'<br />';
-echo 'Data after decrypting back: '.$new->decrypt($dataToDecrypt).'<br />';
+$wordToEncrypt = 'This is to be encrypted';
+
+$cipher = 'aes-256-cbc';
+try {
+    $newEncryption = new Encryption($cipher);
+    $wordToDecrypt = $newEncryption->encrypt($wordToEncrypt);
+    echo 'Encrypted word: '.$wordToDecrypt.'<br />';
+    echo 'Result after decrypting: '.$newEncryption->decrypt($wordToDecrypt).'<br />';
+
+} catch (Exception $e) {
+    echo $e->getMessage().' '.'<br />';
+}
+
+
 
 
